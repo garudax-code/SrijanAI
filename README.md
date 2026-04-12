4e9d28df9149db415ebdac1a85d0c7e6f14bceed
garudax-code <iamthegarud1362@gmail.com>
2026-04-12 21:38:54 +0530

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SrijanAI
+<<<<<<< HEAD
 SrijanAI is a AI based print on demand website
+=======
+This is an AI-based print on demand website
+>>>>>>> a63324c (Initial setup and error handling)
---
+++ backend/src/index.ts
@@ -0,0 +1,38 @@
+import "dotenv/config";
+import express, {Request, Response} from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { HTTPSTATUS } from "./config/http.config";
+import { Env } from "./config/env.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { BadRequestException } from "./utils/app-error";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+
+const app = express();
+
+app.use(
+    cors({
+        origin:[Env.FRONTEND_ORIGIN],
+        methods:["GET","POST","PUT","DELETE"],
+        credentials:true,
+    })
+)
+
+app.use(express.json({limit: "10mb" }))
+app.use(cookieParser())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/health", asyncHandler(async (req: Request, res: Response) => {
+  res.status(HTTPSTATUS.OK).json({
+    message: "Server is healthy",
+    status: "Ok"
+  })
+
+}))
+
+app.use(errorHandler)
+
+app.listen(Env.PORT, async () => {
+  //await connectDatabase()
+  console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
+})
